44214bcf7d849e58c7c8b03cd36a81de39262663
Tolani Afolabi <tolaniafolabi24@gmail.com>
2018-06-26 18:16:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function shout(hello) {
+  return 'HELLO'
+}
+function whisper (HELLO){
+  return 'hello'
+}
+function logShout(hello){
+  console.log('HELLO')
+}
+function logWhisper(HELLO){
+  console.log('hello')
+}
+function sayHiToGrandma(hello){
+  return "I can't hear you!"
+}
+
